9ddf2a9be9ef2f748efc29dd9c8c020ed96dbb94
andreendo <andreendo@gmail.com>
2018-03-15 11:02:44 -0300

    adding escritorCSV

---
+++ src/main/java/br/utfpr/tdd/ex1/EscritorCSV.java
@@ -0,0 +1,48 @@
+package br.utfpr.tdd.ex1;
+
+import java.io.BufferedWriter;
+import java.math.RoundingMode;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+/**
+ *
+ * @author andreendo
+ */
+class EscritorCSV {
+    CSVPrinter csvPrinter;
+    public EscritorCSV(String filePath) {
+        try {
+            BufferedWriter writer = Files.newBufferedWriter(Paths.get(filePath));
+
+            csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
+                .withHeader("RA", "Nome", "NF", "Situacao"));
+            csvPrinter.flush();            
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    void escrever(String ra, String nome, double notaFinal, String situacao) {
+        try {
+            Locale locale  = new Locale("en", "UK");
+            DecimalFormat df = (DecimalFormat)
+                                NumberFormat.getNumberInstance(locale);
+            df.applyPattern(".#");
+            df.setRoundingMode(RoundingMode.DOWN);
+            csvPrinter.printRecord(ra, nome, df.format(notaFinal), situacao);
+            csvPrinter.flush();            
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
---
+++ src/test/java/br/utfpr/tdd/ex1/EscritorCSVTest.java
@@ -0,0 +1,37 @@
+package br.utfpr.tdd.ex1;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import org.apache.commons.io.FileUtils;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+/**
+ *
+ * @author andreendo
+ */
+public class EscritorCSVTest {
+    
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+    
+    @Test
+    public void testEscrita() throws IOException {
+        File tempFolder = folder.newFolder();
+        EscritorCSV escritor = new EscritorCSV(tempFolder.getAbsolutePath() + "/saida.csv");
+        escritor.escrever("1234", "Nome do aluno1", 7.555, "aprovado");
+        escritor.escrever("12345", "Nome do aluno3", 1.43, "reprovado por falta");
+        
+        File csvFileCreated = new File(tempFolder.getAbsolutePath() + "/saida.csv");
+        assertTrue( csvFileCreated.exists() );
+        
+        List<String> lines = FileUtils.readLines(csvFileCreated, "UTF-8");
+        assertEquals(3, lines.size());
+        assertEquals("RA,Nome,NF,Situacao", lines.get(0));
+        assertEquals("1234,Nome do aluno1,7.5,aprovado", lines.get(1));
+        assertEquals("12345,Nome do aluno3,1.4,reprovado por falta", lines.get(2));
+    }
+}
