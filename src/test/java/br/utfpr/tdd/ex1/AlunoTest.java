30a2a7ec115258a5bc2c8547f60c75673c7e8adf
tamirismalacrida <tamiris_malacrida@hotmail.com>
2018-05-21 20:54:51 -0300

    Teste de Refatoração
    Refatoração

--- src/test/java/br/utfpr/tdd/ex1/AlunoTest.java
+++ src/test/java/br/utfpr/tdd/ex1/AlunoTest.java
@@ -73,12 +73,11 @@ public class AlunoTest {
     public void testNotaFinalMaximo6(){
         Aluno aluno = new Aluno("RA", "Tamiris");
         
-        aluno.setNotaProjeto(8.0);
+        aluno.setNotaProjeto(6.0);
         aluno.setNotaRAA(5.0);
         aluno.addNotaAtividadePratica(8.0);  
-                     
-        double vNF = aluno.getNF();
-        assertEquals(6.0, vNF, 1);
+          
+        assertEquals(6.0, aluno.getNF(), 1);
         
     }
 }
